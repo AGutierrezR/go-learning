7ca511000867afa494a37a77f5079122d0e8a427
Andres Gutierrez <agutierrez.codes@gmail.com>
2024-03-23 13:40:11 +0100

    Variables, Strings & Numbers

--- main.go
+++ main.go
@@ -2,6 +2,44 @@ package main
 
 import "fmt"
 
+var someName = "Yuji"
+// otherName := "Gojo" not working
+
 func main()  {
-	fmt.Println("Hello world!")
+	// Strings	
+	// var nameOne string = "Mario"
+	// var nameTwo = "Luigi"
+	// var nameThree string
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameOne = "Peach"
+	// nameThree = "Bowser"
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameFour := "Yoshi"
+
+	// fmt.Println(nameOne, nameTwo, nameThree, nameFour)
+	// fmt.Println(someName)
+
+	// ints
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// bits & memory
+	var numOne int8 = 127 // int8 from -128 to 127
+	var numTwo int8 = -128 // int8 from -128 to 127
+	var numThree uint = 25 // unit are positive number
+
+	fmt.Println(numOne, numTwo, numThree)
+
+	var scoreOne float32 = 25.98 // floatings number 
+	var scoreTwo float64 = 888888999.7 // large floating nubmers
+
+	fmt.Println(scoreOne, scoreTwo)
+
 }
