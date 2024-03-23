311729d7a9a1b1b6196d6e1eecb648b9e581e127
Andres Gutierrez <agutierrez.codes@gmail.com>
2024-03-23 14:52:40 +0100

    Arrays & Slices

--- main.go
+++ main.go
@@ -3,27 +3,30 @@ package main
 import "fmt"
 
 func main()  {
-	age := 35
-	name := "Andres"
 
-	// Print (no new line)
-	fmt.Print("Hello, ")
-	fmt.Print("world \n")
-	fmt.Print("new line \n")
+	// Arrays (are fixed size)
+	// var ages [3]int = [3]int{2, 27, 35}
+	var ages = [3]int{2, 27, 35}
+	names := [4]string{"Yoshi", "Mario", "Peach", "Bowser"}
+	names[1] = "Luigi"
 
-	// Pringln (add a new line automatly)
-	fmt.Println("Hello world!")
-	fmt.Println("goodbye ninjas!")
-	fmt.Println("my age is", age, "and my name is", name) // no need for concatenation
+	fmt.Println(ages, len(ages))
+	fmt.Println(names, len(names))
+	
+	// Slices (can be manipulated)
+	var scores = []int{100, 50, 60}
+	scores[2] = 25
+	scores = append(scores, 85)
 
-	// Printf (formatted string) %_ format specifier
-	fmt.Printf("My age is %v and name is %v \n", age, name)
-	fmt.Printf("My age is %q and name is %q \n", age, name) // %q is for strings, number will be '#'
-	fmt.Printf("age is of type %T \n", age)
-	fmt.Printf("your scored %f points! \n", 2.255)
-	fmt.Printf("your scored %0.1f points! \n", 2.255)
+	fmt.Println(scores, len(scores))
 
-	// Sprintf (save formatted strings)
-	var str  = fmt.Sprintf("My age is %v and name is %v", age, name)
-	println(str)
+	// Slice ranges
+	rangeOne := names[1:3] // include the first number but no the second
+	rangeTwo := names[2:] // from the first number until the dend
+	rangeThree := names[:3] // include the first number but no the second
+
+	fmt.Println(rangeOne, rangeTwo, rangeThree)
+
+	rangeOne = append(rangeOne, "Koopa")
+	fmt.Println(rangeOne)
 }
