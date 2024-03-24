93877a2254bc72cda8fd6014aa703f1d43d1a678
Andres Gutierrez <agutierrez.codes@gmail.com>
2024-03-24 01:27:17 +0100

    Maps

--- main.go
+++ main.go
@@ -1,15 +1,44 @@
 package main
 
-import "fmt"
-
-var score = 99.5
+import (
+	"fmt"
+)
 
 func main() {
-	sayHello("Mario")
 
-	for _, v := range points {
-		fmt.Println(v)
+	menu := map[string]float64{
+		"soup": 4.99,
+		"pie": 6.99,
+		"salad": 6.99,
+		"toffe pudding": 3.55,
 	}
 
-	showScore()
+	fmt.Println(menu)
+	fmt.Println(menu["soup"])
+	
+	// Looping maps
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	// Ints as keys type
+	phonebook := map[int]string {
+		267584967: "mario",
+		984759373: "luigi",
+		845775485: "peach",
+	}
+		
+	// Geting a value by key
+	fmt.Println(phonebook[267584967])
+
+	// looping
+	for k, v := range phonebook {
+		fmt.Println(k, "-", v)
+	}
+
+	// Modifying a value by key
+	phonebook[984759373] = "bowser"
+
+	fmt.Println(phonebook)
+	
 }
