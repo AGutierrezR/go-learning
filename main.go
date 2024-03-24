85fc161fb2cd8fa8f2914171f65e5b6819a5950d
Andres Gutierrez <agutierrez.codes@gmail.com>
2024-03-24 01:55:59 +0100

    Structs & Custom Types

--- main.go
+++ main.go
@@ -4,22 +4,10 @@ import (
 	"fmt"
 )
 
-func updateName(x *string) {
-	*x = "wedge"
-}
-
-
 func main() {
-	name := "tifa"
+	
+	mybill := newBill("Mario's bill")
 
-	// updateName(name)
+	fmt.Println(mybill)
 
-	// fmt.Println("memory address of name is:", &name)
-	m := &name
-	fmt.Println("memory address:", m)
-	fmt.Println("value at memory address:", *m)
-
-	fmt.Println(name)
-	updateName(m)
-	fmt.Println(name)
 }
