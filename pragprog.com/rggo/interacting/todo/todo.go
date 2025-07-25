efe5667fba655481c66fa84cc9b61304d8bde909
Andres Gutierrez <agutierrez.codes@gmail.com>
2025-07-25 11:45:20 +0200

    Add List methods [todo]
    The (l *List) before the name is a receiver type and associate the
    method with the type. In this case, we'll be able to do
    list.(Add,Complete,Delete,Save,Get)
    
    The error after the function `name()`, is to express that the function
    is going to return an error

--- pragprog.com/rggo/interacting/todo/todo.go
+++ pragprog.com/rggo/interacting/todo/todo.go
@@ -21,3 +21,67 @@ type item struct {
 // List represents a list of ToDo items. It is a slice of item structs
 // The first letter is upper case to make it "public" to the package
 type List []item
+
+// Add creates a new todo item and appends it to the list
+func (l *List) Add(task string) {
+	t := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+
+	*l = append(*l, t)
+}
+
+// Complete method marks a ToDo item as completed by
+// setting Done = true and CompletedAt to the current time
+func (l *List) Complete(i int) error {
+	ls := *l
+
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("Item %d does not exist", i)
+	}
+
+	ls[i-1].Done = true
+	ls[i-1].CompletedAt = time.Now()
+
+	return nil
+}
+
+func (l *List) Delete(i int) error {
+	ls := *l
+
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("Item %d does not exist", i)
+	}
+
+	// Adjusting index for 0 based indexing
+	*l = append(ls[:i-1], ls[i:]...)
+
+	return nil
+}
+
+// Save method encodes the List as JSON and saves it // using the provided file name
+func (l *List) Save(filename string) error {
+	js, err := json.Marshal(l)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filename, js, 0644)
+}
+
+// Get method opens the provided file name, decodes // the JSON data and parses it into a List
+func (l *List) Get(filename string) error {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+	if len(file) == 0 {
+		return nil
+	}
+	return json.Unmarshal(file, l)
+}
